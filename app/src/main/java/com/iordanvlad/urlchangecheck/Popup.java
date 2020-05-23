ef929bffa312f265b2f0c9d007fb775f865de6c2
Vlad <iordanvlad36@gmail.com>
2020-05-23 21:03:00 +0300

    Hashing and url requests for multiple protocols.

--- app/src/main/java/com/iordanvlad/urlchangecheck/Popup.java
+++ app/src/main/java/com/iordanvlad/urlchangecheck/Popup.java
@@ -1,17 +1,32 @@
 package com.iordanvlad.urlchangecheck;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class Popup extends Activity {
@@ -19,6 +34,7 @@ public class Popup extends Activity {
     private static final String TAG = "Popup class";
     private static final String FIELDS_ERROR = "Both fields are required!";
     private static final String DUPLICATE_ERROR = "This URL is already monitored!";
+    private static final String INVALID_URL_ERROR = "This URL is invalid!";
     ArrayList<String> url_data_list = new ArrayList<>();
     int position;
     String operation;
@@ -36,13 +52,16 @@ public class Popup extends Activity {
         Button submit = findViewById(R.id.add_url);
         Button delete = findViewById(R.id.delete_url);
 
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
         setPopupSize();
         initializePopup();
 
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(validateInputs()){
+                if(validateInputs() && isInternetPermissionGranted()){
                     updateUrlList();
                 }
             }
@@ -86,23 +105,30 @@ public class Popup extends Activity {
         String name = url_name.getText().toString();
         String url = url_value.getText().toString();
 
+        String html = getHtmlWithProtocols(url);
+        String hash = md5(html);
+        int size = html.length();
+
         try {
             if (operation.equals("edit")) {
                 JSONObject json = new JSONObject(url_data_list.get(position));
                 json.put("name", name);
                 json.put("url", url);
+                json.put("hash", hash);
+                json.put("size", size);
                 url_data_list.set(position, json.toString());
             } else {
                 JSONObject json = new JSONObject();
                 json.put("name", name);
                 json.put("url", url);
-                json.put("hash", "undefined");
-                json.put("size", 0);
+                json.put("hash", hash);
+                json.put("size", size);
                 url_data_list.add(json.toString());
             }
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
 
         Intent returnIntent = new Intent();
         returnIntent.putExtra("url_data_list", url_data_list);
@@ -126,7 +152,11 @@ public class Popup extends Activity {
         String name = url_name.getText().toString();
         String url = url_value.getText().toString();
 
-        if (name.isEmpty() || url.isEmpty()) {
+
+        if (!URLUtil.isValidUrl(url)){
+            url_error.setText(INVALID_URL_ERROR);
+            return false;
+        } else if (name.isEmpty() || url.isEmpty()) {
             url_error.setText(FIELDS_ERROR);
             return false;
         } else if (checkUrl(url) >= 0 && !operation.equals("edit")) {
@@ -163,5 +193,93 @@ public class Popup extends Activity {
         int height = (int) (dm.heightPixels * 0.45);
 
         getWindow().setLayout(width, height);
+    }
+
+    public String getSourceHtml(String url) {
+        try {
+            URLConnection connection = (new URL(url)).openConnection();
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+            connection.connect();
+
+            // Read and store the result line by line then return the entire string.
+            InputStream in = connection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder html = new StringBuilder();
+            for (String line; (line = reader.readLine()) != null; ) {
+                html.append(line);
+            }
+            in.close();
+            return html.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public String getHtmlWithProtocols(String url) {
+        String html = "";
+        ArrayList<String> protocols = new ArrayList<>();
+        protocols.add("https://");
+        protocols.add("http://");
+        protocols.add("ftp://");
+
+        html = getSourceHtml(url);
+        if (html.equals("")) {
+            for (String protocol : protocols) {
+                String[] url_split = url.split("//", 2);
+                url = url_split[url_split.length - 1];
+                html = getSourceHtml(protocol + url);
+                if (!html.equals("")) {
+                    return html;
+                }
+            }
+        }
+
+        return html;
+    }
+
+    public String md5(String s) {
+        try {
+            // Create MD5 Hash
+            MessageDigest digest = java.security.MessageDigest.getInstance("MD5");
+            digest.update(s.getBytes());
+            byte messageDigest[] = digest.digest();
+
+            // Create Hex String
+            StringBuffer hexString = new StringBuffer();
+            for (byte b : messageDigest) hexString.append(Integer.toHexString(0xFF & b));
+            return hexString.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return "";
+    }
+
+    public boolean isInternetPermissionGranted() {
+        if (checkSelfPermission(Manifest.permission.INTERNET)
+                == PackageManager.PERMISSION_GRANTED) {
+            Log.v(TAG, "Permission is granted1");
+            return true;
+        } else {
+
+            Log.v(TAG, "Permission is revoked1");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            Log.d(TAG, "Internet");
+            if(grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                Log.v(TAG,"Permission: "+permissions[0]+ "was "+grantResults[0]);
+                //resume tasks needing this permission
+                updateUrlList();
+            }
+        }
     }
 }
